3f83839f2e0e93062903ca6bbe8464eab023a936
h1r0yuki8823 <hiroyukiandswift@gmail.com>
2018-05-01 08:19:38 +0900

    use bodyParser

--- index.js
+++ index.js
@@ -4,11 +4,28 @@ var app = express();
 
 
 app.engine('ejs' ,ejs.renderFile);
+app.use(express.static('public'));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.get("/", (req, res) => {
-    res.render('index.ejs',
-        {title: 'Index',
-            content: 'This is Express-app Top page!'});
+   var msg = 'This is Index Page!<br>'
+        + '※メッセージを書いて送信してください。'
+    res.render('index.ejs',{
+        title: 'Index', 
+        content:msg,
+    });
+});
+
+app.post('/',(req,res) => {
+    var msg = 'This is Posted Page!<br>' +
+        'あなたは「<b>' + req.body.message +
+        '</b>」と送信しました。';
+    res.render('index.ejs',{
+        title:'Posted',
+        content:msg
+    });
 });
 
 var server = app.listen(3000,() => {
